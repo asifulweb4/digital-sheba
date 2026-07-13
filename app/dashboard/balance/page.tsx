667f863c957e12370388b5f70d59124c09c125cd
asifulweb4 <asifulweb4@gmail.com>
2026-07-13 19:36:32 +0600

    balance update

--- app/dashboard/balance/page.tsx
+++ app/dashboard/balance/page.tsx
@@ -34,7 +34,7 @@ export default function RechargePage() {
     const maxWithdraw = 10000;
 
     const paymentNumbers = {
-        bKash: "01349739116",
+        bKash: "01350084246",
         Nagad: "01625400505"
     };
 
